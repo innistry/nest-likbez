624f0557f8fcaf35b1c3b8e5aa6013d528e41fb8
Ilya Naryshkin <inaryshkin@mentalstack.com>
2019-08-30 20:20:48 +0300

    Middleware

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { LoggerMiddleware } from '../logger.middleware';
 
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
   exports: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(CatsController);
+  }
+}
