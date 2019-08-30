ae9f52fb9e2ce2c6f69bcb3f24b7059b829f3026
Ilya Naryshkin <inaryshkin@mentalstack.com>
2019-08-30 19:23:31 +0300

    Providers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Header, HttpCode, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+    return this.catsService.create(createCatDto);
+  }
+
   @Get()
-  @HttpCode(200)
-  async findAll(@Query() createCatDto: CreateCatDto): Promise<string> {
-    return 'This action returns all cats ' + createCatDto.name;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
